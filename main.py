4b88b93a76fb154c36d4bed1be6394a4576d66c9
antoshkina <130902637+antoshkina@users.noreply.github.com>
2023-04-16 21:29:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Игровое поле
+board = range(1,10)
+def draw_board(board):
+    print("-------------")
+    for i in range(3):
+        print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+        print("-------------")
